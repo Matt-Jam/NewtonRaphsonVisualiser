4783aa9028f854a4c6408461cc7b8096efc5ac4d
Matt-Jam <mattyjamson@gmail.com>
2023-10-27 00:50:20 +0100

    Added visualiser code to method

--- main.py
+++ main.py
@@ -7,45 +7,46 @@ class NewtonRaphson:
     def __init__(self, func: Callable[[float], float], a1: float) -> None:
         self.f = func
         self.a = a1
-
     
     def iterate(self):
         self.a = self.a - (self.func())/(self.deriv())
-        return self.a
 
-    def func(self, h=0):
+    def func(self, h=0) -> float:
         return self.f(self.a+h)
     
-    def deriv(self):
+    def deriv(self) -> float:
         h = 0.0001
         return (self.func(h) - self.func())/h
 
-def sqaure(x: float) -> float:
+    def visualise(self,lower_bound: int =0, upper_bound: int = 10, depth: int = 10) -> None:
+        t = np.linspace(lower_bound, upper_bound, abs(upper_bound-lower_bound)*100)
+        fig, ax = plt.subplots()
+        ax.plot(t,self.f(t))
+        
+        x = []
+        y = []
+
+        for i in range(depth):
+            x += [self.a, self.a]
+            y += [0, self.func()]
+            self.iterate()
+        
+        ax.plot(x,y)
+
+        ax.spines['left'].set_position('zero')
+        ax.spines['right'].set_color('none')
+        ax.yaxis.tick_left()
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['top'].set_color('none')
+        ax.xaxis.tick_bottom()
+
+        fig.show()
+        input()
+
+
+
+def square(x: float) -> float:
     return x**2 - 2*x - 4
 
-test = NewtonRaphson(sqaure,10)
-
-t = np.linspace(-10,10,10000)
-fig, ax = plt.subplots()
-ax.plot(t,sqaure(t))
-x = []
-y = []
-for i in range(10):
-    x += [test.a,test.a]
-    y += [0, test.func()]
-    test.iterate()
-ax.spines['left'].set_position('zero')
-
-ax.plot(x,y)
-# turn off the right spine/ticks
-ax.spines['right'].set_color('none')
-ax.yaxis.tick_left()
-
-# set the y-spine
-ax.spines['bottom'].set_position('zero')
-
-# turn off the top spine/ticks
-ax.spines['top'].set_color('none')
-ax.xaxis.tick_bottom()
-fig.show()
-input()
+test = NewtonRaphson(square,10)
+test.visualise(-20,20,10)
