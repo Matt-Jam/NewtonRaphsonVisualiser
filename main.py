ab216c3bc3a4c78955c6d40ebd59962a2284397b
Matt-Jam <mattyjamson@gmail.com>
2023-10-27 11:19:52 +0100

    Added div by zero fail safe

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from typing import Callable
 import matplotlib.pyplot as plt
 import numpy as np
-
+import math
 class NewtonRaphson:
 
-    def __init__(self, func: Callable[[float], float], a1: float) -> None:
+    def __init__(self, func: Callable[[float], float]) -> None:
         self.f = func
-        self.a = a1
+        self.a = 0
     
     def iterate(self):
         self.a = self.a - (self.func())/(self.deriv())
@@ -18,18 +18,26 @@ class NewtonRaphson:
         h = 0.0001
         return (self.func(h) - self.func())/h
 
-    def visualise(self,lower_bound: int =0, upper_bound: int = 10, depth: int = 10) -> None:
+    def visualise(self,a1: int,lower_bound: int =0, upper_bound: int = 10, depth: int = 10) -> None:
+        self.a = a1
         t = np.linspace(lower_bound, upper_bound, abs(upper_bound-lower_bound)*100)
         fig, ax = plt.subplots()
-        ax.plot(t,self.f(t))
+        ax.plot(t, list(map(self.f, t)))
         
         x = []
         y = []
 
         for i in range(depth):
+            p = self.func()
+            if ((p > upper_bound * 2) or ((p < lower_bound * 2))):
+                break
             x += [self.a, self.a]
             y += [0, self.func()]
-            self.iterate()
+            
+            try:
+                self.iterate()
+            except ZeroDivisionError:
+                break
         
         ax.plot(x,y)
 
@@ -48,5 +56,11 @@ class NewtonRaphson:
 def square(x: float) -> float:
     return x**2 - 2*x - 4
 
-test = NewtonRaphson(square,10)
-test.visualise(-20,20,10)
+def t1(x: float) -> float:
+    if x == 0:
+        return 0
+    
+    return x/math.sqrt(abs(x))
+test = NewtonRaphson(t1)
+
+test.visualise(2,-3,3,10)
